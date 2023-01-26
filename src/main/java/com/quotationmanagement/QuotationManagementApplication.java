63a1d6521989bbd07fd20879605e621821f6988a
Leonardo <leonardo_scalabrini@hotmail.com>
2023-01-26 11:12:08 -0300

    Format code

--- src/main/java/com/quotationmanagement/QuotationManagementApplication.java
+++ src/main/java/com/quotationmanagement/QuotationManagementApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuotationManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuotationManagementApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(QuotationManagementApplication.class, args);
+  }
 }
